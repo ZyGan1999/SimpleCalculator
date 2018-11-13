8ea13b7140a420c426d1b1304bde7b76b477e20d
泽宇 甘 <2017202047@ruc.edu.cn>
2018-11-14 00:30:30 +0800

    eigenvalue(?)

--- Calculator/Matrix.cpp
+++ Calculator/Matrix.cpp
@@ -1,6 +1,8 @@
 #include "Matrix.h"
 #include <stdlib.h>
+#include <cmath>
 #include <iostream>
+#include <map>
 using namespace std;
 Matrix::Matrix()
 {
@@ -9,7 +11,7 @@ Matrix::Matrix()
 	colNum = 0;
 }
 
-Matrix::Matrix(int mat[][100], int row, int col)
+Matrix::Matrix(double mat[][100], int row, int col)
 {
 	//data = (int**)malloc(100000 * sizeof(int));
 	rowNum = row;
@@ -17,6 +19,17 @@ Matrix::Matrix(int mat[][100], int row, int col)
 	for (int i = 0; i < rowNum; ++i) {
 		for (int j = 0; j < colNum; ++j) {
 			data[i][j] = mat[i][j];
+		}
+	}
+}
+
+Matrix::Matrix(int row, int col)
+{
+	rowNum = row;
+	colNum = col;
+	for (int i = 0; i < rowNum; ++i) {
+		for (int j = 0; j < colNum; ++j) {
+			data[i][j] = 0;
 		}
 	}
 }
@@ -59,8 +72,19 @@ void Matrix::minus(const Matrix & mat)
 	}
 }
 
-void Matrix::multiply(const Matrix & mat)
+Matrix * Matrix::multiply(const Matrix & mat)
 {
+	auto rtn = new Matrix(this->rowNum, mat.colNum);
+	for (int ci = 0; ci < rtn->rowNum; ci++) {
+		for (int cj = 0; cj < rtn->colNum; cj++) {
+			int result = 0;
+			for (int pos = 0; pos < this->rowNum; pos++) {
+				result += this->data[ci][pos] * mat.data[pos][cj];
+			}
+			rtn->data[ci][cj] = result;
+		}
+	}
+	return rtn;
 }
 
 void Matrix::Transpose()
@@ -84,8 +108,151 @@ void Matrix::Transpose()
 
 double Matrix::CalcEigenvalue()
 {
-	return 0.0;
+	double pdbEigenValues[100] = { 0 };
+	double dbEps = 0.00001f;
+	int nJt = 1000;
+	auto pMatrix = new Matrix(this->data, rowNum, colNum);
+	auto pdblVects = new Matrix(rowNum, colNum);
+	//
+	for (int i = 0; i < rowNum; i++)
+	{
+		pdblVects->data[i][i] = 1.0f;
+		for (int j = 0; j < rowNum; j++)
+		{
+			if (i != j)
+				pdblVects->data[i][j] = 0.0f;
+		}
+	}
+
+	int nCount = 0;		//迭代次数
+	while (1)
+	{
+		//在pMatrix的非对角线上找到最大元素
+		double dbMax = pMatrix->data[0][1];
+		int nRow = 0;
+		int nCol = 1;
+		for (int i = 0; i < rowNum; i++)			//行
+		{
+			for (int j = 0; j < rowNum; j++)		//列
+			{
+				double d = fabs(pMatrix->data[i][j]);
+
+				if ((i != j) && (d> dbMax))
+				{
+					dbMax = d;
+					nRow = i;
+					nCol = j;
+				}
+			}
+		}
+
+		if (dbMax < dbEps)     //精度符合要求 
+			break;
+
+		//if (nCount > nJt)       //迭代次数超过限制
+		//	break;
+
+		nCount++;
+
+		double dbApp = pMatrix->data[nRow][nRow];
+		double dbApq = pMatrix->data[nRow][nCol];
+		double dbAqq = pMatrix->data[nCol][nCol];
+
+		//计算旋转角度
+		double dbAngle = 0.5*atan2(-2 * dbApq, dbAqq - dbApp);
+		double dbSinTheta = sin(dbAngle);
+		double dbCosTheta = cos(dbAngle);
+		double dbSin2Theta = sin(2 * dbAngle);
+		double dbCos2Theta = cos(2 * dbAngle);
+
+		pMatrix->data[nRow][nRow] = dbApp * dbCosTheta*dbCosTheta +
+			dbAqq * dbSinTheta*dbSinTheta + 2 * dbApq*dbCosTheta*dbSinTheta;
+		pMatrix->data[nCol][nCol] = dbApp * dbSinTheta*dbSinTheta +
+			dbAqq * dbCosTheta*dbCosTheta - 2 * dbApq*dbCosTheta*dbSinTheta;
+		pMatrix->data[nRow][nCol] = 0.5*(dbAqq - dbApp)*dbSin2Theta + dbApq * dbCos2Theta;
+		pMatrix->data[nCol][nRow] = pMatrix->data[nRow][nCol];
+
+		for (int i = 0; i < rowNum; i++)
+		{
+			if ((i != nCol) && (i != nRow))
+			{
+				//int u = i * nDim + nRow;	//p  
+				//int w = i * nDim + nCol;	//q
+				dbMax = pMatrix->data[i][nRow];
+				pMatrix->data[i][nRow] = pMatrix->data[i][nCol] * dbSinTheta + dbMax * dbCosTheta;
+				pMatrix->data[i][nCol] = pMatrix->data[i][nCol] * dbCosTheta - dbMax * dbSinTheta;
+			}
+		}
+
+		for (int j = 0; j < rowNum; j++)
+		{
+			if ((j != nCol) && (j != nRow))
+			{
+				//int u = nRow * nDim + j;	//p
+				//int w = nCol * nDim + j;	//q
+				dbMax = pMatrix->data[nRow][j];
+				pMatrix->data[nRow][j] = pMatrix->data[nCol][j] * dbSinTheta + dbMax * dbCosTheta;
+				pMatrix->data[nCol][j] = pMatrix->data[nCol][j] * dbCosTheta - dbMax * dbSinTheta;
+			}
+		}
+
+		//计算特征向量
+		for (int i = 0; i < rowNum; i++)
+		{
+			//int u = i * nDim + nRow;		//p   
+			//int w = i * nDim + nCol;		//q
+			dbMax = pdblVects->data[i][nRow];
+			pdblVects->data[i][nRow] = pdblVects->data[i][nCol] * dbSinTheta + dbMax * dbCosTheta;
+			pdblVects->data[i][nCol] = pdblVects->data[i][nCol] * dbCosTheta - dbMax * dbSinTheta;
+		}
+		pMatrix->print();
+	}
+
+	//对特征值进行排序以及重新排列特征向量,特征值即pMatrix主对角线上的元素
+	std::map<double, int> mapEigen;
+	for (int i = 0; i < rowNum; i++)
+	{
+		pdbEigenValues[i] = pMatrix->data[i][i];
+
+		mapEigen.insert(make_pair(pdbEigenValues[i], i));
+	}
+
+	double *pdbTmpVec = new double[rowNum*rowNum];
+	std::map<double, int>::reverse_iterator iter = mapEigen.rbegin();
+	for (int j = 0; iter != mapEigen.rend(), j < rowNum; ++iter, ++j)
+	{
+		for (int i = 0; i < rowNum; i++)
+		{
+			pdbTmpVec[i*rowNum + j] = pdblVects->data[i][iter->second];
+		}
+
+		//特征值重新排列
+		pdbEigenValues[j] = iter->first;
+	}
+
+	//设定正负号
+	for (int i = 0; i < rowNum; i++)
+	{
+		double dSumVec = 0;
+		for (int j = 0; j < rowNum; j++)
+			dSumVec += pdbTmpVec[j * rowNum + i];
+		if (dSumVec<0)
+		{
+			for (int j = 0; j < rowNum; j++)
+				pdbTmpVec[j * rowNum + i] *= -1;
+		}
+	}
+
+	memcpy(pdblVects, pdbTmpVec, sizeof(double)*rowNum*rowNum);
+	delete[]pdbTmpVec;
+	for (auto i : mapEigen) {
+		cout << ((i.first < dbEps) ? (0) : (i.first)) << ' ';
+	}
+	cout << endl;
+	return 1;
 }
+	//
+
 
 double Matrix::CalcDeterminant()
 {
--- Calculator/Matrix.h
+++ Calculator/Matrix.h
@@ -3,11 +3,12 @@ class Matrix
 {
 public:
 	Matrix();
-	Matrix(int mat[][100], int row, int col);
+	Matrix(double mat[][100], int row, int col);
+	Matrix(int row, int col);
 	Matrix(const Matrix & mat);
 	void add(const Matrix & mat);//矩阵加法
 	void minus(const Matrix & mat);//矩阵减法
-	void multiply(const Matrix & mat);//矩阵乘法
+	Matrix * multiply(const Matrix & mat);//矩阵乘法
 	void Transpose();//转置
 	double CalcEigenvalue();//计算特征值
 	double CalcDeterminant();//计算行列式的值
@@ -23,6 +24,6 @@ public:
 private:
 	int rowNum;
 	int colNum;
-	int data[100][100];
+	double data[100][100];
 };
 
